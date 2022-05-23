764d291be1a334521702096b3921ea07f21e53a0
AtenaNoimos <atena.fadaei@noimos.ai>
2022-05-23 12:35:19 +0200

    feat: add dataset config file

---
+++ src/custom.py
@@ -0,0 +1,59 @@
+dataset_info = dict(
+    dataset_name='coco',
+    paper_info=dict(
+        author='Lin, Tsung-Yi and Maire, Michael and '
+        'Belongie, Serge and Hays, James and '
+        'Perona, Pietro and Ramanan, Deva and '
+        r'Doll{\'a}r, Piotr and Zitnick, C Lawrence',
+        title='Microsoft coco: Common objects in context',
+        container='European conference on computer vision',
+        year='2014',
+        homepage='http://cocodataset.org/',
+    ),
+    keypoint_info={
+        0:
+        dict(
+            name='lower_left',
+            id=0,
+            color=[0, 0, 255],
+            type='lower',
+            swap='lower_right'),
+        1:
+        dict(
+            name='top_left',
+            id=1,
+            color=[188, 255, 0],
+            type='upper',
+            swap='top_right'),
+        2:
+        dict(
+            name='top_right',
+            id=2,
+            color=[0, 255, 137],
+            type='upper',
+            swap='top_left'),
+        3:
+        dict(
+            name='lower_right',
+            id=3,
+            color=[255, 0, 0],
+            type='lower',
+            swap='lower_left')
+    
+    },
+    skeleton_info={
+        0:
+        dict(link=('lower_left', 'top_left'), id=0, color=[0, 255, 0]),
+        1:
+        dict(link=('top_left', 'top_right'), id=1, color=[0, 255, 0]),
+        2:
+        dict(link=('top_right', 'lower_right'), id=2, color=[0, 255, 0]),
+        3:
+        dict(link=('lower_right', 'lower_left'), id=3, color=[0, 255, 0])
+    },
+    joint_weights=[
+        1., 1., 1., 1.
+    ],
+    sigmas=[
+        0.025, 0.025, 0.025, 0.025
+    ])
